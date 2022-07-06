c146403b3dc7c708f72699835882c40738f35fcf
dimass31 <nikitenkovdmitriy@yandex.ru>
2022-07-06 21:13:24 +0300

    to vercel

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
-from app import app as application
-#, ssl_context=application.context
-#host='0.0.0.0'
+# from index import app as application
+# #, ssl_context=application.context
+# #host='0.0.0.0'
  
-if __name__ == "__main__":
-    context = ('/etc/nginx/ssl/dancinginfire.crt', '/etc/nginx/ssl/dancinginfire.key')
-    application.run(host='dancinginfire.ru', port=5000, debug=True, ssl_context=context)
+# if __name__ == "__main__":
+#     context = ('/etc/nginx/ssl/dancinginfire.crt', '/etc/nginx/ssl/dancinginfire.key')
+#     application.run(host='dancinginfire.ru', port=5000, debug=True, ssl_context=context)
